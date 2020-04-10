c9934cf7a7d478173c8d11ecbe438b68f705dcb7
Rollpopptery <richard@tctec.net>
2020-04-10 18:03:42 +1000

    Update steering.ino

--- va1/steering.ino
+++ va1/steering.ino
@@ -23,6 +23,113 @@ extern int DIGITAL_STEER_DIR ;
 const int CLOCKWISE = 1;
 const int ANTICLOCK = 0;
 
+
+
+// single 'singleton' class with the state of the steering,
+// Also, control functions for Steering
+//
+class Steering{ 
+
+
+  public:
+  
+  // State of the steering 
+  //
+  // if this is greater than 0, then the motor is moving
+  //
+  static int motorSpeed  ;
+  static int motorDirection  ;
+
+  // timer, shows the start time of the current movement
+  //
+  static unsigned long timer  ;
+  static unsigned long duration ;
+
+ 
+  
+  static void Stop();
+  static void StartMove(int dir, int Speed, unsigned long timeout);
+  
+  
+  
+};
+
+
+
+//
+// called repeatedly from main task loop, this is the Steering 'Thread'
+//
+void Steering_StateMachine()
+{
+  // is Steering moving ? , monitor the timeout
+  //
+  if (Steering::motorSpeed > 0)
+  {
+    if ( (unsigned long)(millis() - Steering::timer) > Steering::duration)
+    {
+      // finished move / timeout
+      //
+      Steering::Stop();    
+      
+    }
+  }
+
+  
+  
+  
+}
+
+void Steering::Stop()
+{
+  
+  digitalWrite(DIGITAL_STEER_MOTOR, 1);
+
+  // update the state to 'stopped'
+  //
+  motorSpeed = 0;
+  
+  
+}
+
+
+
+// start the motor moving
+// provide a time / timeout.
+// dir is the Direction
+//
+void Steering::StartMove(int dir, int Speed, unsigned long timeout)
+{
+
+  // set the direction , and start the motor
+  //
+  Steering::timer = millis();
+  Steering::duration = timeout;
+  
+  digitalWrite(DIGITAL_STEER_DIR, dir);
+  digitalWrite(DIGITAL_STEER_MOTOR, 1);
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---- ignore below here
+
+
 // generic move function, includes breaking pause
 //
 // dir = direction
